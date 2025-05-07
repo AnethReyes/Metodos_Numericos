9de0b3f6c79ef7db095ef7613acf20c066b0401a
AnethReyes <23030286@itesa.edu.mx>
2025-05-06 21:13:31 -0600

    Delete Problema_4.py

--- Problema_4.py
+++
@@ -1,5 +0,0 @@
-import math
-num = math.sqrt(2)  # Raíz cuadrada exacta
-aprox = round(num, 4)  # Redondeado a 4 decimales
-error_redondeo = num - aprox
-print(f'Error de redondeo: {error_redondeo}')
