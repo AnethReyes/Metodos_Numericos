a9b61d257c73fb3d61c596c96b496109b542193a
AnethReyes <23030286@itesa.edu.mx>
2025-05-27 18:14:30 -0600

    Update Problema_3.py

--- TEMA 1/Problema_3.py
+++ TEMA 1/Problema_3.py
@@ -2,3 +2,10 @@ num = 0.1 + 0.2  # Suma de flotantes en Python
 aprox = round(num, 1)  # Redondeo a 1 decimal
 error_redondeo = num - aprox
 print(f'Error de redondeo: {error_redondeo}')
+
+# === Ejemplo de ejecución ===
+# Input:
+# num = 0.1 + 0.2
+#
+# Output:
+# Error de redondeo: 5.551115123125783e-17
