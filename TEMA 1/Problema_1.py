ed38548fd119f565e1e6f7b843b7ce434f9ce2c0
AnethReyes <23030286@itesa.edu.mx>
2025-05-27 18:09:16 -0600

    Update Problema_1.py

--- TEMA 1/Problema_1.py
+++ TEMA 1/Problema_1.py
@@ -2,3 +2,9 @@ num = 3.1415926535  # Número original
 aprox = round(num, 2)  # Redondeado a 2 decimales
 error_redondeo = num - aprox
 print(f'Error de redondeo: {error_redondeo}')
+
+### Input del código
+
+```plaintext
+Error de redondeo: 0.0015926534999999298
+```
