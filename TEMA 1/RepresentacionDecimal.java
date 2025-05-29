7b16c6828a5cdc1ea2a28942b9dadd273125e963
AnethReyes <23030286@itesa.edu.mx>
2025-05-29 10:04:39 -0600

    Update and rename Problema_5.py to RepresentacionDecimal.java

---
+++ TEMA 1/RepresentacionDecimal.java
@@ -0,0 +1,15 @@
+public class RepresentacionDecimal {
+    public static void main(String[] args) {
+        double num = 0.1;  // Número original
+        System.out.printf("Número almacenado en Java: %.17f%n", num);
+    }
+}
+
+
+/** # === Ejemplo de ejecución ===
+* Input:
+* num = 0.1
+*
+* Output:
+* Número almacenado en Python: 0.10000000000000001 */
+
